dc6041e77eafc45ab4dc0ec52d4a23c561aaa185
Anika H. <74740342+AnHan42@users.noreply.github.com>
2024-07-17 17:22:14 +0200

    Update server_client_classification.sh

--- cluster_bash_scripts/server_client_classification.sh
+++ cluster_bash_scripts/server_client_classification.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 
-#arguments $1 no of runs
+#arguments $1 no of runs $2 OKRA/FLAKE $3 base port
 
-# Loop i times
-for i in $(seq 1 $1)
+# List of feature values
+#no_inputparties=("5" "6" "7" "8" "9" "10")
+no_inputparties=("3")
+
+#no_features=("128" "512" "2048" "8192" "32768" "131072" "524288")
+no_features=("512")
+
+# privacy parameter k: 10 random values for 10 runs
+#random_values=$(python -c "import random; random.seed(42); print(' '.join(map(str, [random.randint(1, 900) for _ in range(10)])))")
+
+# Loop through feature values
+for parties in "${no_inputparties[@]}"
 do
+    # Loop for specified number of runs
+    for i in $(seq 1 $1)
+		
+    do        
+		# get k
+		#k=$(echo $random_values | cut -d' ' -f$i)
 
-# Start Server.py
-base_port=8000
-base_features=200
-port=$((base_port+i-1))
-features=$((base_features*i))
+        # Start Server.py
+        base_port=$3
+        port=$((base_port+i+100))
 
-python server_classification.py --num_clients 3 --base_port $port --FL False &
+        python -u server_classification.py --num_clients $parties --base_port $port --n_samples 400 &
 
-sleep 2
+        sleep 4 
 
-# Start Alice
-python client_classification.py --mask "FLAKE" --n_features $features --k 10 --party_id 1 --n_samples 200 --base_port $port &
+        # Start clients
+        for party_id in $(seq 1 $parties)
+        do
+            python -u client_classification.py --mask $2 --n_features 30 --k 1 --party_id $party_id --n_samples 400 --base_port $port --max_parties $parties
+            sleep 0.1 
+        done
 
-sleep 2
-
-# Start Bob
-python client_classification.py --mask "FLAKE" --n_features $features --k 10 --party_id 2 --n_samples 200 --base_port $port &
-
-#sleep 2
-
-# Start Charlie
-python client_classification.py --mask "FLAKE" --n_features $features --k 10 --party_id 3 --n_samples 200 --base_port $port &
-
-
-wait
-
+        wait
+    done
 done
